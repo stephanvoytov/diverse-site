6d1f8de9f5b2acd5c95319ab047ee489b8d781f8
Stepan Voytov <voytov.st.vi@gmail.com>
2026-07-23 10:25:00 +0200

    fix: align CSP with TinaCMS docs (add s3, assets.tina.io, identity-v2)

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,12 @@
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 const csp = [
   `default-src 'self'`,
-  `img-src 'self' data: https://mc.yandex.ru https://vk.com https://tile.openstreetmap.org https://purecatamphetamine.github.io https://us-assets.i.posthog.com`,
+  `img-src 'self' data: https://mc.yandex.ru https://vk.com https://tile.openstreetmap.org https://purecatamphetamine.github.io https://us-assets.i.posthog.com https://assets.tina.io`,
   `script-src 'self' 'unsafe-eval' 'unsafe-inline' https://mc.yandex.ru https://vk.com https://us-assets.i.posthog.com`,
   `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
   `font-src 'self' data: https://fonts.gstatic.com https://fonts.googleapis.com`,
   `frame-src 'self' https://*.tina.io https://*.tinajs.io`,
-  `connect-src 'self' https://mc.yandex.ru https://vk.com https://tile.openstreetmap.org https://*.tinajs.io https://content.tinajs.io https://identity.tinajs.io https://assets.tinajs.io https://app.tina.io https://cognito-idp.us-east-1.amazonaws.com https://us-assets.i.posthog.com https://us.i.posthog.com`,
+  `connect-src 'self' https://mc.yandex.ru https://vk.com https://tile.openstreetmap.org https://*.tinajs.io https://content.tinajs.io https://identity.tinajs.io https://identity-v2.tinajs.io https://assets.tinajs.io https://app.tina.io https://cognito-idp.us-east-1.amazonaws.com https://s3.us-east-1.amazonaws.com https://us-assets.i.posthog.com https://us.i.posthog.com`,
   `form-action 'self'`,
 ].join("; ");
 
